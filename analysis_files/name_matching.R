90af7d83f283a7e5722f1a9cc73e2fa4aa1376fb
fcampelo <fcampelo@ufmg.br>
2018-05-05 00:46:42 -0300

    Some more work on name matching routine

--- analysis_files/name_matching.R
+++ analysis_files/name_matching.R
@@ -6,19 +6,40 @@ alias.list    <- read.csv("metaphor_aliases.csv", header = TRUE, sep = ";",
 # Get matches for each entry (based on the alias list)
 title.matches <- character(nrow(alias.list))
 for (i in seq(title.matches)){
-  # Target patterns for the i-th metaphor
-  targets <- unlist(strsplit(gsub(", ", ",", alias.list$aliases[i]), 
-                             split = ","))
   matches <- numeric()
-  if(length(targets)){
+  
+  # Target aliases for the i-th metaphor
+  targets.alias <- unlist(strsplit(gsub(", ", ",", alias.list$aliases[i]), 
+                                   split = ","))
+  if(length(targets.alias)){
     # for each alias
-    for (j in seq(targets)){
-      # get paper titles that match the particular
-      matches <- c(matches, grep(pattern = targets[j], 
+    for (j in seq(targets.alias)){
+      # get paper titles that match the alias
+      matches <- c(matches, grep(pattern = targets.alias[j], 
                                  x = all.papers$title, 
                                  ignore.case = TRUE))
     }
   }
+  
+  # Target acronyms for the i-th metaphor
+  targets.acronym <- unlist(strsplit(gsub(", ", ",", alias.list$acronyms[i]), 
+                                   split = ","))
+  if(length(targets.acronym)){
+    # for each alias
+    for (j in seq(targets.acronym)){
+      # get paper titles that match the acronym
+      #
+      # Claus, what is the regexp to match the following query?
+      # "targets.acronym[j], surrounded by non-alphanumeric characters"
+      # 
+      # my.pattern <- 
+      # matches <- c(matches, grep(pattern = my.pattern, 
+      #                            x = all.papers$title, 
+      #                            ignore.case = TRUE))
+    }
+  }
+  
+  
   # Remove possible double matches and collapse everything into a single string
   title.matches[i] <- paste0(unique(matches), 
                              collapse = ",") 
