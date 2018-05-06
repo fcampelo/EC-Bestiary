43024c7d60d1900dfa67f1e4b4cb7da159646f07
fcampelo <fcampelo@ufmg.br>
2018-05-05 23:19:04 -0300

    Some more development on title matching routing

--- analysis_files/name_matching.R
+++ analysis_files/name_matching.R
@@ -4,8 +4,10 @@ alias.list    <- read.csv("metaphor_aliases.csv", header = TRUE, sep = ";",
                           stringsAsFactors = FALSE)
 
 # Get matches for each entry (based on the alias list)
-title.matches <- character(nrow(alias.list))
+title.matches <- vector(mode = "list", length = nrow(alias.list))
 for (i in seq(title.matches)){
+  cat("\nProcessing title matches: ", 
+      sprintf("%03d of %d", i, nrow(alias.list)))
   matches <- numeric()
   
   # Target aliases for the i-th metaphor
@@ -21,26 +23,38 @@ for (i in seq(title.matches)){
     }
   }
   
-  # Target acronyms for the i-th metaphor
-  targets.acronym <- unlist(strsplit(gsub(", ", ",", alias.list$acronyms[i]), 
-                                   split = ","))
-  if(length(targets.acronym)){
-    # for each alias
-    for (j in seq(targets.acronym)){
-      # get paper titles that match the acronym
-      #
-      # Claus, what is the regexp to match the following query?
-      # "targets.acronym[j], surrounded by non-alphanumeric characters"
-      # 
-      # my.pattern <- 
-      # matches <- c(matches, grep(pattern = my.pattern, 
-      #                            x = all.papers$title, 
-      #                            ignore.case = TRUE))
-    }
-  }
+  # # Target acronyms for the i-th metaphor
+  # targets.acronym <- unlist(strsplit(gsub(", ", ",", alias.list$acronyms[i]), 
+  #                                  split = ","))
+  # if(length(targets.acronym)){
+  #   # for each alias
+  #   for (j in seq(targets.acronym)){
+  #     # get paper titles that match the acronym
+  #     #
+  #     # Claus, what is the regexp to match the following query?
+  #     # "targets.acronym[j], surrounded by non-alphanumeric characters"
+  #     # 
+  #     # my.pattern <- 
+  #     # matches <- c(matches, grep(pattern = my.pattern, 
+  #     #                            x = all.papers$title, 
+  #     #                            ignore.case = TRUE))
+  #   }
+  # }
   
   
   # Remove possible double matches and collapse everything into a single string
-  title.matches[i] <- paste0(unique(matches), 
-                             collapse = ",") 
+  title.matches[[i]] <- unique(matches)
 }
+
+
+# For each entry, get distribution of citations per year
+title.matches <- lapply(title.matches,
+                        function(x, all.papers){
+                          pub.years <- all.papers$year[x]
+                          pubs.by.year  <- table(pub.years)
+                          return(list(match.indx = x,
+                                      pub.years = pub.years,
+                                      pubs.by.year  = pubs.by.year))
+                        }, 
+                        all.papers = all.papers)
+names(title.matches) <- alias.list$metaphor
